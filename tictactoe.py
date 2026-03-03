1647f3cdc1e49d9b5c2995c8c30d9c718f541fcc
Burhan Faisal <kh.m.burhan@gmail.com>
2026-03-03 21:55:08 +0500

    Added score tracking function

--- tictactoe.py
+++ tictactoe.py
@@ -8,6 +8,8 @@ board = [
 X='X'
 O='O'
 
+score_x,score_y=0,0
+
 def print_board():
     line ="---+---+---"
     print(line)
@@ -62,7 +64,24 @@ def is_winner():
     if (board[0][0]==board[1][1]==board[2][2] !=' ' or
         board[0][2]==board[1][1]==board[2][0] !=' '):
         return True
-    
+
+def next_game():
+    another_game=input("Want to play another game? yes /no").strip().lower()
+    if another_game=='yes':
+        return True
+    else:
+        return False
+def update_score(current):
+    if current==X:
+        global score_x
+        global score_y
+        score_x+=1
+    else:
+        score_y+=1
+
+def reset_board():
+    return [[' ']*3 for _ in range(3)]
+
 def main():
     print("Let's play the Tic_Tac_Toe🤩")
     print_board()
@@ -70,17 +89,30 @@ def main():
     while True:
         print(f"Player {current_player}'s turn ")
         get_move(current_player)
-
         print_board()
 
         if(is_winner()):
             print(f'Player{current_player} is the WINNNER🥳')
-            break
-    
+            update_score(current_player)
+            if(next_game()):
+                global board
+                board=reset_board()
+                continue
+            else:
+                print(f"No of games won by X are {score_x}")
+                print(f"No of games won by O are {score_y}")
+                break   
         if (is_fullboard()):
             print(f'Match Tied')
-            break
+            if(next_game()):
+                global board
+                board=reset_board()
+                continue
+            else:
+                break   
         current_player=O if current_player==X else X
+
+
 
 main()
 
