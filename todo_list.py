4c4d114e463eaca6811d0e3dba822e03f98a9819
Burhan Faisal <kh.m.burhan@gmail.com>
2026-03-07 13:44:38 +0500

    Category Feature Added
    It would also handle previous user session and also fixed some bugs.

--- todo_list.py
+++ todo_list.py
@@ -1,5 +1,5 @@
+import termcolor,json
 
-import termcolor,json,os
 pending_tasks=[]
 completed_tasks=[]
 todo_list_menu=[
@@ -8,40 +8,46 @@ todo_list_menu=[
     "Remove a Task",
     "Mark tasks as completed",
     "View Completed Tasks",
+    "Change Category",
+    "Add Another Category",
     "Exit"
 ]
+categories={
 
+}
 
-choices=[1,2,3,4,5,6]
+choices=[1,2,3,4,5,6,7,8]
 Task_file="todo_list.json"
 
-def view_comp_tasks():
-    if completed_tasks:
-        for index,task in enumerate(completed_tasks,start=1):
-            print(index,'. ',task)
-        return True
-    else:
-        print("No tasks in the list.")
-        return False
-    
-
-def view_tasks():
-    if pending_tasks:
-        for index,task in enumerate(pending_tasks,start=1):
+def view_comp_tasks(category):
+    completed=categories[category]['Completed Tasks']
+    if completed:
+        for index,task in enumerate(completed,start=1):
             print(index,'. ',task)
         return True
     else:
         print("No tasks in the list.")
         return False
 
-def remove_task():
-        if(view_tasks()):
+
+def view_pending_tasks(category):
+    pending=categories[category]['Pending Tasks']
+    if pending:
+        for index,task in enumerate(pending,start=1):
+            print(index,'. ',task)
+        return True
+    else:
+        print("No tasks in the list.")
+        return False
+
+def remove_task(category):
+        if(view_pending_tasks(category)):
             while True:
                 try:
                     task_no=int(input("Enter the task number you want to remove: "))
 
-                    if 1<=task_no<=len(pending_tasks):
-                        pending_tasks.pop(task_no-1)
+                    if 1<=task_no<=len(categories[category]['Pending Tasks']):
+                        categories[category]['Pending Tasks'].pop(task_no-1)
                         save_tasks()
                         print('Task no.',termcolor.colored(task_no,'red'),'is removed')
                         break
@@ -51,13 +57,28 @@ def remove_task():
                     print(e)
 
 
-def mark_as_completed():
-    if(view_tasks()):
+def choose_category():
+    if categories:
+        for index,category_item in enumerate(categories,start=1):
+            print(f"{index}.{category_item}")
+    else:
+        termcolor.cprint("Starting Fresh No Category yet..",'magenta')
+        return add_category()
+    while True:
+            category=input("Enter your Category Name: ").strip().upper()
+            if category in categories:
+                return category
+            else:
+                print("Invalid Category Name..")
+
+def mark_as_completed(category):
+    if(view_pending_tasks(category)):
         while True:
             try:
                 task_no=int(input("Enter the task no. that is completed: "))
-                if 1<=task_no<=len(pending_tasks):
-                    completed_tasks.append(pending_tasks.pop(task_no-1))
+                completed=categories[category]['Completed Tasks']
+                if 1<=task_no<=len(categories[category]['Pending Tasks']):
+                    completed.append(categories[category]['Pending Tasks'].pop(task_no-1))
                     save_tasks()
                     print('Task no.',termcolor.colored(task_no,'blue'),'is marked as completed')
                     break
@@ -68,50 +89,28 @@ def mark_as_completed():
 
 def save_tasks():
     try:
-        data = {
-            'pending': pending_tasks,
-            'completed': completed_tasks
-        }
         with open(Task_file,'w') as f:
-            json.dump(data,f,indent=4)
+            json.dump(categories,f,indent=4)
     except Exception as e:
         termcolor.cprint("Error in saving tasks",'red')
 
 
 def load_tasks():
     try:
-        global pending_tasks, completed_tasks   
+        global categories
         with open(Task_file,'r') as f:
-            data=json.load(f)
-            pending_tasks = data.get('pending', [])
-            completed_tasks = data.get('completed', [])
-
-            termcolor.cprint("Loaded  pending and completed tasks from previous session", 'magenta',attrs=['underline'])
-
-            termcolor.cprint("Pending Tasks:", 'yellow',attrs=['bold'])
-            if pending_tasks:
-                for index,task in enumerate(pending_tasks,start=1):
-                    print(f"{index}.{task}")
-            else:
-                termcolor.cprint("No completed tasks.", 'yellow')
-
-
-            termcolor.cprint("Completed Tasks:", 'green', attrs=['bold'])
-            if completed_tasks:
-                for index,task in enumerate(completed_tasks,start=1):
-                    print(f"{index}.{task}")
-            else:
-                termcolor.cprint("No completed tasks.", 'magenta')
-
-
+            categories=json.load(f)
+            termcolor.cprint("Successfully Loaded Pending and Completed tasks from previous session", 'yellow',attrs=['bold'])
     except Exception:
-        termcolor.cprint("Some error occurred in loading data ",'red')
+        termcolor.cprint("No previous session",'red')
+        categories={}
 
-def add_task(): 
+
+def add_task(category): 
     while True:
         task=input("Add a task: ").strip()
         if len(task) != 0:
-           pending_tasks.append(task)
+           categories[category]['Pending Tasks'].append(task)
            save_tasks()
            termcolor.cprint("Task Added.",'green')
            break
@@ -119,8 +118,23 @@ def add_task():
             print('Invalid task!')
 
 
+def add_category():
+    while True:
+        category_name=input("Enter Your Category Name for your todos': ").strip().upper()
+        if category_name:
+            categories.update({category_name:{   
+                'Pending Tasks':[],
+                'Completed Tasks':[]
+            }})
+            termcolor.cprint("Category Added..",'green')
+            return category_name
+        else:
+            termcolor.cprint("Invalid Category Name",'red')
+        
+
 def main():
     load_tasks()
+    category=choose_category()
     print("Todo-List Menu :")
     for index,list in enumerate(todo_list_menu,start=1):
         print(index,'. ',list)
@@ -129,15 +143,19 @@ def main():
             choice=int(input("Enter Your Choice:").strip())
             if choice in choices:
                 if choice == 1:
-                    view_tasks()
+                    view_pending_tasks(category)
                 elif choice==2:
-                    add_task()
+                    add_task(category)
                 elif choice==3:
-                    remove_task()
+                    remove_task(category)
                 elif choice==4:
-                    mark_as_completed()
+                    mark_as_completed(category)
                 elif choice==5:
-                    view_comp_tasks()
+                    view_comp_tasks(category)
+                elif choice==6:
+                    category=choose_category()
+                elif choice==7:
+                    add_category()
                 else:
                     save_tasks()
                     exit()
@@ -146,5 +164,5 @@ def main():
         except ValueError:
             print("Enter a valid choice")
 
-
-main()
+if __name__=='__main__':
+    main()
